3262caa885d2cc711f0a2550855c67c0658e56e4
nalapatt <70668968+nalapatt@users.noreply.github.com>
2021-08-09 10:10:10 -0400

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,15 @@
+variable "aws_region" {
+  description = "The AWS region to create things in."
+  default     = "us-west-2"
+}
+
+variable "key_name" {
+  description = " SSH keys to connect to ec2 instance"
+  default     =  "aws-new-project-key"
+}
+
+variable "instance_type" {
+  description = "instance type for ec2"
+  default     =  "t2.micro"
+}
+                    
